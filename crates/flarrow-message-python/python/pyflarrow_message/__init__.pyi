45d533fe696b5613c3f82b15e1777cb250521acc
Enzo Le Van <dev@enzo-le-van.fr>
2025-05-02 20:02:40 +0200

    feat: pyflarrow_message

--- crates/flarrow-message-python/python/pyflarrow_message/__init__.pyi
+++ crates/flarrow-message-python/python/pyflarrow_message/__init__.pyi
@@ -1,6 +1,13 @@
 import pyarrow as pa
 
+from typing import TypeVar, Type, Optional
+
+T = TypeVar("T", bound="ArrowMessage")
+
 class ArrowMessage:
     @classmethod
-    def from_arrow(cls, data: pa.Array) -> "ArrowMessage":
+    def from_arrow(cls: Type[T], arrow_array: pa.Array) -> Optional[T]:
+        pass
+
+    def to_arrow(self) -> pa.Array:
         pass
